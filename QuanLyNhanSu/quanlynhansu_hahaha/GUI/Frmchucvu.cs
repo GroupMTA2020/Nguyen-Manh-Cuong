7e752d9cfa9bc3575265a18d227a7309fcb06ff2
ManhCuong <cuongtop4598@gmail.com>
2020-09-07 00:04:20 +0700

    add form main and form login , account

---
+++ QuanLyNhanSu/quanlynhansu_hahaha/GUI/Frmchucvu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyNhanSu.DAO;
+using QuanLyNhanSu.DATAHA;
+
+namespace QuanLyNhanSu.GUI
+{
+    public partial class Frmchucvu : Form
+    {
+   
+        public Frmchucvu()
+        {
+            InitializeComponent();
+        }
+     
+
+        private void Frmchucvu_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void btntimkiem_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dgvthongtin_SelectionChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
