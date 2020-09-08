2d8b47bba09a44584c37417240a2b979a1f6bce5
nganninh <trieuninhngan2012@gmail.com>
2020-09-08 10:04:43 +0700

    updateclass
    chỉnh class cơ bản

--- QuanLyNhanSu/quanlynhansu_hahaha/DATAHA/QuanLyNhanSuDbContext.cs
+++ QuanLyNhanSu/quanlynhansu_hahaha/DATAHA/QuanLyNhanSuDbContext.cs
@@ -1,4 +1,4 @@
-﻿namespace QuanLyNhanSu.DATAHA
+namespace QuanLyNhanSu.DATAHA
 {
     using System;
     using System.Data.Entity;
@@ -10,6 +10,42 @@
         public QuanLyNhanSuDbContext()
             : base("name=QuanLyNhanSuDbContext")
         {
+        }
+
+        public virtual DbSet<CHUCVU> CHUCVUs { get; set; }
+        public virtual DbSet<DANTOC> DANTOCs { get; set; }
+        public virtual DbSet<NGOAINGU> NGOAINGUs { get; set; }
+        public virtual DbSet<NHANVIEN> NHANVIENs { get; set; }
+        public virtual DbSet<PHONGBAN> PHONGBANs { get; set; }
+        public virtual DbSet<THANNHAN> THANNHANs { get; set; }
+        public virtual DbSet<TONGIAO> TONGIAOs { get; set; }
+        public virtual DbSet<TRINHDOHOCVAN> TRINHDOHOCVANs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CHUCVU>()
+                .Property(e => e.KIHIEUCHUCVU)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<NHANVIEN>()
+                .Property(e => e.MANV)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<NHANVIEN>()
+                .Property(e => e.MATKHAU)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<NHANVIEN>()
+                .Property(e => e.TAIKHOAN)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<PHONGBAN>()
+                .Property(e => e.KIHIEUPHONG)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<THANNHAN>()
+                .Property(e => e.MATN)
+                .IsUnicode(false);
         }
     }
 }
