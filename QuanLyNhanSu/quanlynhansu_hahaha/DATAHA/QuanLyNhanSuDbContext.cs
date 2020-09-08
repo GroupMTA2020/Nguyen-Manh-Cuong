3894fdd3da314dd23481c76bd2f048d2ddbee719
ManhCuong <cuongtop4598@gmail.com>
2020-09-08 10:54:54 +0700

    add database context using entity framework

---
+++ QuanLyNhanSu/quanlynhansu_hahaha/DATAHA/QuanLyNhanSuDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace QuanLyNhanSu.DATAHA
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class QuanLyNhanSuDbContext : DbContext
+    {
+        public QuanLyNhanSuDbContext()
+            : base("name=QuanLyNhanSuDbContext")
+        {
+        }
+    }
+}
