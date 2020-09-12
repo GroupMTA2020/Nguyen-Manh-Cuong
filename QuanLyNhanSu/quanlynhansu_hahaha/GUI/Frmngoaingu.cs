19987d2e14b375ba9652fb2025e0f4a0007789dc
NguyenQuocKhanh <44228638+kaanblack09@users.noreply.github.com>
2020-09-12 21:00:17 +0700

    1
    hiển thị thông tin trình độ ngoại ngữ lên view form ngoại ngữ

--- QuanLyNhanSu/quanlynhansu_hahaha/GUI/Frmngoaingu.cs
+++ QuanLyNhanSu/quanlynhansu_hahaha/GUI/Frmngoaingu.cs
@@ -14,12 +14,42 @@ namespace QuanLyNhanSu.GUI
 {
     public partial class Frmngoaingu : Form
     {
-
+        private QuanLyNhanSuDbContext db = DBService.db;
+        private int index = 0, index1 = 0;
         private void Frmngoaingu_Load(object sender, EventArgs e)
         {
-      
+            txtghichu.Enabled = false;
+            txtten.Enabled = false;
+            Loadthongtin();
         }
+        private void Loadthongtin()
+        {
+            int i = 0;
+            string keyword = txtTimKiem.Text;
+            var dbNV = db.NGOAINGUs.ToList()
+                       .Select(p => new
+                       {
 
+                           STT = ++i,
+                           ID = p.ID,
+                           Ten = p.TEN,
+                           Ghichu = p.GHICHU,
+
+                       }).ToList()
+
+                       ;
+
+            dgvthongtin.DataSource = dbNV.Where(p => p.Ten.Contains(keyword)).ToList();
+
+            // cập nhật index 
+            index = index1;
+            try
+            {
+                dgvthongtin.Rows[index].Cells["STT"].Selected = true;
+                dgvthongtin.Select();
+            }
+            catch { }
+        }
         public Frmngoaingu()
         {
             InitializeComponent();
